846b092e5f92521f2d835a5bb3788865fa55dc5e
Mehrsary <Mehrsary@gmail.com>
2024-04-12 08:48:33 +0000

    feat: init flame node chart dependencies

--- flame/0_setup.sh
+++ flame/0_setup.sh
@@ -1,7 +1,36 @@
-helm dependency update
-helm install flame ./flame
+# Update helm dependencies
+helm dependency update .
 
+# Get namespace from command line argument or user input
+namespace=${1:-}
+if [ -z "$namespace" ]; then
+    read -p "Enter namespace (default is 'default'): " namespace
+fi
+namespace=${namespace:-default}
 
-helm upgrade flame ./flame
+kubectl get namespace $namespace > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Namespace does not exist. Creating..."
+    kubectl create namespace $namespace
+fi
 
-helm uninstall flame
+# Get action from command line argument or user input
+action=${2:-}
+if [ -z "$action" ]; then
+    read -p "Enter action (install, upgrade, uninstall): " action
+fi
+
+case $action in
+    install)
+        helm install flame-node . --namespace $namespace
+        ;;
+    upgrade)
+        helm upgrade flame-node . --namespace $namespace
+        ;;
+    uninstall)
+        helm uninstall flame-node --namespace $namespace
+        ;;
+    *)
+        echo "Invalid action. Please enter 'install', 'upgrade', or 'uninstall'."
+        ;;
+esac
